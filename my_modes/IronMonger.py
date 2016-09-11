c11307204f4094961bd9f4d9c046f47020b567a3
epthegeek <epthegeek@gmail.com>
2016-09-11 15:34:21 -0500

    working on monger letter logic with spinners and orbits

--- my_modes/IronMonger.py
+++ my_modes/IronMonger.py
@@ -16,6 +16,11 @@ class IronMonger(procgame.game.AdvancedMode):
                              self.game.lamps['mongerG'],
                              self.game.lamps['mongerE'],
                              self.game.lamps['mongerR']]
+        self.delay_names = {0,'leftSpinner',
+                            1, 'centerSpinner',
+                            2, 'rightSpinner',
+                            3, 'leftOrbit',
+                            4, 'rightOrbit'}
 
 
     def evt_ball_starting(self):
@@ -25,9 +30,10 @@ class IronMonger(procgame.game.AdvancedMode):
         self.battles = self.game.getPlayerState('monger_battles')
         self.status = self.game.getPlayerState('monger_status')
         # for locking out spinners based on conditions
-        self.valid = [True,True,True]
+        self.valid = [True,True,True,True,True]
         self.toy_valid = True
         self.toy_letters = self.game.getPlayerState('toy_letters')
+        self.orbit_quiet = False
 
     def evt_ball_ending(self):
         self.game.setPlayerState('monger_letters', self.letters)
@@ -46,18 +52,32 @@ class IronMonger(procgame.game.AdvancedMode):
 
     def sw_leftSpinner_active(self,sw):
         if self.valid[0]:
-            self.valid[0] = False
-            self.spinner_hit(0)
+            self.set_valid_switches(0)
+            self.letter_hit(0)
 
     def sw_centerSpinner_active(self,sw):
         if self.valid[1]:
-            self.valid[1] = False
-            self.spinner_hit(1)
+            self.set_valid_switches(1)
+            self.letter_hit(1)
 
     def sw_rightSpinner_active(self,sw):
         if self.valid[2]:
-            self.valid[2] = False
-            self.spinner_hit(2)
+            self.set_valid_switches(2)
+            self.letter_hit(2)
+
+    def sw_leftOrbit_active(self,sw):
+        if self.valid[3]:
+            self.set_valid_switches(3)
+            self.letter_hit(3)
+        # play the orbit noise
+        self.orbit_noise()
+
+    def sw_rightOrbit_active(self,sw):
+        if self.valid[4]:
+            self.set_valid_switches(4)
+            self.letter_hit(4)
+        # play the orbit noise
+        self.orbit_noise()
 
     def sw_mongerOptoLeft_active(self,sw):
         if self.toy_valid:
@@ -74,9 +94,7 @@ class IronMonger(procgame.game.AdvancedMode):
             self.toy_valid = False
             self.hit_toy()
 
-    def spinner_hit(self,spinner):
-        # reset the valid in a bit
-        self.delay(delay=2,handler=lambda:self.validate(spinner))
+    def letter_hit(self):
         if self.letters < 10:
             self.letters += 1
             if self.letters == 10:
@@ -125,3 +143,55 @@ class IronMonger(procgame.game.AdvancedMode):
                     self.monger_lamps[n].enable()
                 else:
                     pass
+
+    def set_valid_switches(self,switch):
+        # left spinner
+        if switch == 0:
+            # affects left spinner and left orbit
+            self.process_validation([0,3])
+        # center spinner
+        elif switch == 1:
+            # affects only itself
+            self.process_validation([1])
+        # right spinner
+        elif switch == 2:
+            # affects right spinner and right orbit
+            self.process_validation([2,4])
+        # left orbit
+        elif switch == 3:
+            # affect the left spinner and the right orbit
+            self.process_validation([0,4])
+            # forces re-validation of the right spinner in case of a repeat fast loop
+            self.validate(2)
+        # right orbit
+        elif switch == 4:
+            # affects the left orbit and the right spinner
+            self.process_validation([3,2])
+            # forces re-validation of the left spinner in case of a repeat fast loop
+            self.validate(0)
+
+    def process_validation(self,list):
+        self.stop_valid_reset(list)
+        self.invalidate_switches(list)
+        self.revalidate(list)
+
+    def invalidate_switches(self,list):
+        for item in list:
+            self.valid[item] = False
+
+    def stop_valid_reset(self,list):
+        for item in list:
+            self.cancel_delayed(self.delay_names[item])
+
+    def revalidate(self,list):
+        for item in list:
+            self.delay(self.delay_names[item],delay=2,handler=lambda: self.validate(item))
+
+    def orbit_noise(self):
+        if not self.orbit_quiet:
+            self.orbit_quiet = True
+            self.game.sound.play('helicopter')
+            self.delay(delay=1,handler=self.orbit_noise_reset)
+
+    def orbit_noise_reset(self):
+        self.orbit_quiet = False
